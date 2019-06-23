21a4ad40143736dc8d5cd97c19c5c9037043509a
scarletgv <33155764+scarletgv@users.noreply.github.com>
2019-06-23 13:10:48 -0300

    Update TP3node.py

--- code/TP3node.py
+++ code/TP3node.py
@@ -19,6 +19,8 @@ TOPOREQ = 6
 KEYFLOOD = 7
 TOPOFLOOD = 8
 RESP = 9
+# Máximo de vizinhos que o servent pode se conectar
+MAX_VIZINHOS = 10
 
 def leBanco(arquivo):
     chaveValor = {}
@@ -26,7 +28,7 @@ def leBanco(arquivo):
     with open(arquivo, "r" ) as banco:
         for linha in banco:
             if linha[0] != '#':
-                chaveValor[linha.split(None, 1)[0]] = linha.split(" ", 1)[1]            
+                chaveValor[linha.split(None, 1)[0]] = linha.split(" ", 1)[1].rstrip()           
     return chaveValor
 
 # Servents podem enviar mensagens de flood, resp e id
@@ -53,18 +55,13 @@ def criaRespMsg(valor, nseq):
 
 def leResp(s):
     # Lê nseq
-    msg = s.recv(4)
-    msgUnp = struct.unpack("!i", msg)
-    nseqRecv = msgUnp[0]    
+    msg = s.recv(4)  
     # Lê tamanho
     msg = s.recv(2)
     msgUnp = struct.unpack("!h", msg)
     tamanho = msgUnp[0]    
     # Lê valor, baseado no tamanho
     msg = s.recv(tamanho)
-    msgUnp = struct.unpack("!%ds"%(tamanho), msg)
-    valor = str(msgUnp[0], 'ascii')    
-    print("Resp - valor: "+valor)  
     
 def leFlood(s):
     msg = s.recv(2)
@@ -88,15 +85,13 @@ def leFlood(s):
     tamanho = msgUnp[0]    
     msg = s.recv(tamanho)
     msgUnp = struct.unpack("!%ds"%(tamanho), msg)
-    info = str(msgUnp[0], 'ascii')    
-    print("Flood - TTL: "+str(TTL)+", nseq: "+str(nseqRecv)+", IP: "+IP_ORIG+", PORTO: "+str(PORTO_ORIG)+", INFO: "+info)    
+    info = str(msgUnp[0], 'ascii')      
     return TTL, nseqRecv, IP_ORIG, PORTO_ORIG, info
 
 def leTopoReq(s):
     msg = s.recv(4)
     msgUnp = struct.unpack("!i", msg)
     nseqRecv = msgUnp[0]
-    print("TopoReq - nseq: "+str(nseqRecv))
     return nseqRecv
 
 def leKeyReq(s):
@@ -109,7 +104,6 @@ def leKeyReq(s):
     msg = s.recv(tamanho)
     msgUnp = struct.unpack("!%ds"%(tamanho), msg)
     chave = str(msgUnp[0], 'ascii')    
-    print("KeyReq - nseq: "+str(nseqRecv)+", chave: "+chave)    
     return nseqRecv, chave
 
 def leIDmsg(s):
@@ -117,29 +111,22 @@ def leIDmsg(s):
     msgUnp = struct.unpack("!h", msg)
     porto = msgUnp[0]    
     if porto == 0:
-        print("IDmsg de servent")
+        print("Recebeu ID de servent em "+str(s.getpeername()))
         return 'servent', 0
     else:
-        print("IDmsg - porto: "+str(porto))
+        print("Recebeu ID de cliente no porto "+str(porto))
         return 'client', porto
     
 def alagamento(msg, s, conn):
-    print("Iniciando alagamento.")
     for i in conn:
         if i is not server and i is not s:
-        #if i.getpeername() in servents and i is not s:
-            print("Enviando alagamento para "+str(i.getpeername()))
+            print("Enviando mensagem de alagamento para "+str(i.getpeername()))
             i.send(msg)
-        #if i is not server:
-        #    if i is not s:
-        #        print("Enviando mensagem de alagamento.")
-        #        i.send(msg)  
                 
 def conectaCliente(msg, ipC, portoC):
-    print("Conectando com o cliente na porta "+str(portoC))
+    print("Conectando com o cliente no porto "+str(portoC))
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((ipC, portoC))
-    print("Conectado.")
     client.send(msg)
     client.close()
     
@@ -147,28 +134,26 @@ def recebeMsg(s, tipoRecv, i):
     if tipoRecv == IDmsg:
         origem, porto = leIDmsg(s)
         if origem == 'servent':
-            print("Mensagem ID recebida de servent")
             servents.append(s.getpeername())
         else:
-            print("Mensagem ID recebida de um cliente, porto "+str(s.getpeername()))
             clients.append(s.getpeername())
             ports[s.getpeername()] = porto
     elif tipoRecv == KEYREQ:
         nseqRecv, chave = leKeyReq(s)
-        if chave in banco:
-            print("Eu tenho a chave.")
+        if chave in banco:            
             resposta = criaRespMsg(banco[chave], nseqRecv)
             porto = ports[s.getpeername()]
+            print("Servent possui a chave. Enviando resposta para cliente no porto "+str(porto))
             conectaCliente(resposta, '127.0.0.1', porto)
             return resposta
         else:
-            print("Não tenho a chave. Iniciando alagamento de chave.")
+            print("Servent não possui a chave. Iniciando alagamento.")
             porto_orig = ports[s.getpeername()]
             msg = criaFloodMsg(KEYFLOOD, chave, nseqRecv, porto_orig, 3)
             alagamento(msg, s, i)
     elif tipoRecv == TOPOREQ:
         nseqRecv = leTopoReq(s)
-        print("Requisição de topologia recebida. Iniciando alagamento de topologia.")
+        print("Requisição de topologia recebida. Iniciando alagamento.")
         topologia = '127.0.0.1:'+str(portaS)
         porto_orig = ports[s.getpeername()]
         msg = criaFloodMsg(TOPOFLOOD, topologia, nseqRecv, porto_orig, 3)
@@ -178,11 +163,10 @@ def recebeMsg(s, tipoRecv, i):
         return resposta
     elif tipoRecv == KEYFLOOD or tipoRecv == TOPOFLOOD:
         ttl, nseqRecv, ip_orig, porto_orig, info = leFlood(s)
-        ttl -= 1
-        
+        ttl -= 1        
         if porto_orig in mensagens:
             if nseqRecv in mensagens[porto_orig]:
-                print("Mensagem repetida, descartada.")
+                print("Mensagem recebida repetida, descartada.")
                 return False
         else:
             if porto_orig in mensagens:
@@ -192,22 +176,22 @@ def recebeMsg(s, tipoRecv, i):
         if ttl > 0:
             if tipoRecv == TOPOFLOOD:
                 info += ' 127.0.0.1:'+str(portaS)
-                print("Mensagem de alagamento recebida, enviando resposta para "+str(porto_orig))
+                print("Mensagem de alagamento recebida. Enviando resposta para cliente no porto "+str(porto_orig))
                 msg = criaFloodMsg(TOPOFLOOD, info, nseqRecv, porto_orig, ttl)
                 alagamento(msg, s, i)
                 resposta = criaRespMsg(info, nseqRecv)
                 conectaCliente(resposta, ip_orig, porto_orig)
             else:
                 if info in banco:
-                    print("Eu tenho a chave.")
+                    print("Servent possui a chave. Enviando resposta para cliente no porto "+str(porto_orig))
                     resposta = criaRespMsg(banco[info], nseqRecv)
                     conectaCliente(resposta, ip_orig, porto_orig)
                 else:
-                    print("Não possuo a chave, enviando alagamento.")
+                    print("Servent não possui a chave. Iniciando alagamento.")
                     msg = criaFloodMsg(KEYFLOOD, info, nseqRecv, porto_orig, ttl)
                     alagamento(msg, s, i)
     elif tipoRecv == RESP:
-        print("Tipo inválido, descartando mensagem.")
+        print("Mensagem de tipo inválido, descartando mensagem.")
         leResp(s)        
     return False                   
     
@@ -215,18 +199,19 @@ def recebeMsg(s, tipoRecv, i):
     Abre a porta e adiciona seus vizinhos '''
 portaS = int(sys.argv[1])
 arquivoBanco = sys.argv[2]
+banco = leBanco(arquivoBanco)
+print("-- SERVENT - Porta: "+str(portaS)+" --")
 vizinhos = []
 for i in range(3, len(sys.argv)):
     vizinhos.append(sys.argv[i])
-print("Porto: "+str(portaS)+", banco: "+arquivoBanco+"Vizinhos: "+str(vizinhos))
 
+# Criando soquete do servent
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setblocking(0)
 endereco = ('127.0.0.1', portaS)
 server.bind(endereco)
-server.listen(10) # Máx. 10 vizinhos
+server.listen(MAX_VIZINHOS)
 
-banco = leBanco(arquivoBanco)
 mensagens = {}
 clients = []
 servents = []
@@ -238,7 +223,7 @@ message_queues = {}
 for v in vizinhos:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     h, p = v.split(":")
-    print("Conectando ao vizinho "+h+":"+p)
+    print("Conectado ao vizinho "+h+":"+p)
     s.connect((h,int(p)))
     s.setblocking(0)
     inputs.append(s)
@@ -248,28 +233,24 @@ for v in vizinhos:
     
 while inputs:
     try:
-        print("Esperando...")
         readable, writable, _ = select.select(inputs, outputs, inputs)
         for s in readable:
             if s is server:
                 conn, addr = s.accept()
-                print("Nova conexão com "+str(addr))
+                print("Nova conexão em "+str(addr))
                 conn.setblocking(0)
                 inputs.append(conn)
                 message_queues[conn] = queue.Queue()
             else:
-                print("Recebendo mensagem.")
                 msg = s.recv(2)
                 if msg:
                     msgUnp = struct.unpack("!h", msg)
                     tipoRecv = msgUnp[0]
                     mensagem = recebeMsg(s, tipoRecv, inputs)
-                    #if mensagem:
-                    #    message_queues[s].put(mensagem)
                     if s not in outputs:
                         outputs.append(s)
                 else:
-                    print("Fechando conexão com "+str(s.getpeername()))
+                    print("Encerrando conexão em "+str(s.getpeername()))
                     if s in outputs:
                         outputs.remove(s)
                     inputs.remove(s)
@@ -277,7 +258,6 @@ while inputs:
                     del message_queues[s]
         for s in writable:
             try:
-                print("Adicionando mensagem da fila.")
                 next_msg = message_queues[s].get_nowait()
             except queue.Empty:
                 outputs.remove(s)
@@ -286,7 +266,7 @@ while inputs:
                 print("Enviando mensagem para "+str(s.getpeername()))
                 s.send(next_msg)
     except KeyboardInterrupt:
-        print("Finalizando o servidor.")
+        print("\nFinalizando o servidor.")
         for s in inputs:
             s.close()
         server.close()
